adbee0c464275a05776fbfaa7890cbb1b530e3e3
Blastbrean <73199280+Blastbrean@users.noreply.github.com>
2025-09-21 11:07:34 -0700

    fix: Update commit hash in MENU_TITLE and adjust lastTimestamp in timing file

--- Menu.lua
+++ Menu.lua
@@ -60,7 +60,7 @@ local MENU_TITLE = "Linoria V2 | Deepwoken"
 if LRM_UserNote then
 	MENU_TITLE = string.format(
 		"(Commit %s) Linoria V2 | Deepwoken First Release",
-		string.sub("774752f4908e3914f27f91722ea031ca30dbaf01", 1, 6)
+		string.sub("6ad4ab6d45024f4794259278de783506a0db0e85", 1, 6)
 	)
 end
 
