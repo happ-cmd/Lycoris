a8952d61ae1a40bb9ce5ca496577a7f2d1c28729
juanitoelgordito <221791452+juanitoelgordito@users.noreply.github.com>
2025-10-22 15:40:38 -0700

    Refactor animation and hitbox parameters for various actions
    - Modified hitbox dimensions for "Parry" actions in "Cyclone" and other animations.
    - Adjusted timing and properties for "Dodge" and "Parry" actions.
    - Added new animation "RazorBlitzFollowup" and later removed it.
    - Introduced "GazeBox" part with specific properties and subsequently removed it.
    - Updated material properties and other attributes for existing animations.
    - Ensured consistency in hitbox dimensions across different actions.

--- Modules/CrimsonRain.lua
+++ Modules/CrimsonRain.lua
@@ -7,18 +7,9 @@ local Action = getfenv().Action
 return function(self, timing)
 	local distance = self:distance(self.entity)
 	local action = Action.new()
-	action._when = 500
-	if distance >= 20 then
-		action._when = 570
-	end
-	if distance >= 30 then
-		action._when = 670
-	end
-	if distance >= 40 then
-		action._when = 770
-	end
+	action._when = math.min(400 + distance * 7.5)
 	action._type = "Parry"
-	action.hitbox = Vector3.new(35, 20, 50)
+	action.hitbox = Vector3.new(45, 20, 75)
 	action.name = string.format("(%.2f) Dynamic Crimson Rain Timing", distance)
 	return self:action(timing, action)
 end
--- Modules/Scythe.lua
+++ Modules/Scythe.lua
@@ -5,6 +5,7 @@ local Action = getfenv().Action
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
+	local distance = self:distance(self.entity)
 	local hrp = self.entity:FindFirstChild("HumanoidRootPart")
 	if not hrp then
 		return
@@ -13,9 +14,9 @@ return function(self, timing)
 	if hrp:WaitForChild("REP_SOUND_15776883341", 0.1) then
 		timing.pfh = true
 		local action = Action.new()
-		action._when = 400
+		action._when = math.min(300 + distance * 8)
 		action._type = "Parry"
-		action.hitbox = Vector3.new(15, 15, 20)
+		action.hitbox = Vector3.new(20, 15, 30)
 		action.name = "Blood Scythe Timing"
 		return self:action(timing, action)
 	else
