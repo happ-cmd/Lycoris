33536ff0d7c92381cf247662c776ae90a602a259
Blastbrean <blastbrean@waifu.club>
2025-11-01 23:54:30 -0700

    refactor(RisingShadow): Improve code structure and readability in module function

--- Modules/RisingShadow.lua
+++ Modules/RisingShadow.lua
@@ -14,56 +14,61 @@ local ProjectileTracker = getfenv().ProjectileTracker
 ---@module Features.Combat.Defense
 local Defense = getfenv().Defense
 
+---@module Utility.TaskSpawner
+local TaskSpawner = getfenv().TaskSpawner
+
 ---Module function.
 ---@param self AnimatorDefender
 ---@param timing AnimationTiming
 return function(self, timing)
-	local data = Mantra.data(self.entity, "Mantra:RisingSlashShadow{{Rising Shadow}}")
-	local range = data.rush * 3 + data.drift * 2
-	local thrown = workspace:FindFirstChild("Thrown")
-	if not thrown then
-		return
-	end
+	TaskSpawner.spawn("Module_RisingShadow", function()
+		local data = Mantra.data(self.entity, "Mantra:RisingSlashShadow{{Rising Shadow}}")
+		local range = data.rush * 3 + data.drift * 2
+		local thrown = workspace:FindFirstChild("Thrown")
+		if not thrown then
+			return
+		end
 
-	local tracker = ProjectileTracker.new(function(candidate)
-		return candidate.Name == "TRACKER"
-	end)
+		local tracker = ProjectileTracker.new(function(candidate)
+			return candidate.Name == "TRACKER"
+		end)
 
-	task.wait(0.70 - self.rtt())
+		task.wait(0.70 - self.rtt())
 
-	if self:distance(self.entity) <= 14 then
+		if self:distance(self.entity) <= 14 then
+			local action = Action.new()
+			action._type = "Start Block"
+			action._when = 0
+			action.name = "Rising Shadow Close Timing"
+			action.ihbc = true
+			self:action(timing, action)
+
+			local actionTwo = Action.new()
+			actionTwo._type = "End Block"
+			actionTwo._when = 500
+			actionTwo.ihbc = true
+			self:action(timing, actionTwo)
+		end
+
 		local action = Action.new()
-		action._type = "Start Block"
 		action._when = 0
-		action.name = "Rising Shadow Close Timing"
-		action.ihbc = true
-		self:action(timing, action)
+		action._type = "Start Block"
+		action.name = "Rising Shadow Part"
 
 		local actionTwo = Action.new()
-		actionTwo._type = "End Block"
 		actionTwo._when = 500
+		actionTwo._type = "End Block"
 		actionTwo.ihbc = true
-		self:action(timing, actionTwo)
-	end
 
-	local action = Action.new()
-	action._when = 0
-	action._type = "Start Block"
-	action.name = "Rising Shadow Part"
+		local pt = PartTiming.new()
+		pt.uhc = true
+		pt.duih = true
+		pt.fhb = false
+		pt.name = "RisingShadowProjectile"
+		pt.hitbox = Vector3.new(22 + range, 22 + range, 22 + range)
+		pt.actions:push(action)
+		pt.cbm = true
 
-	local actionTwo = Action.new()
-	actionTwo._when = 500
-	actionTwo._type = "End Block"
-	actionTwo.ihbc = true
-
-	local pt = PartTiming.new()
-	pt.uhc = true
-	pt.duih = true
-	pt.fhb = false
-	pt.name = "RisingShadowProjectile"
-	pt.hitbox = Vector3.new(22 + range, 22 + range, 22 + range)
-	pt.actions:push(action)
-	pt.cbm = true
-
-	Defense.cdpo(tracker:wait(), pt)
+		Defense.cdpo(tracker:wait(), pt)
+	end)
 end
